f71201615617e2b218f0939970ea8b1ec37c7c76
GB-Jun <windgb369@hotmail.com>
2022-08-30 10:39:09 +0800

    typeAnnotation

---
+++ second/typeAnnotation.ts
@@ -0,0 +1,11 @@
+
+// 基本型別(Primitive Types):          number, string, boolean, undefined, null, ES6 的symbol, 在函式中的 void
+// 物件型別(Object Types):
+//          基礎物件:                  Object, Array, JSON, Class, Instance
+//          TSC擴充物件:               Enum, Tuple
+//          函式型別(Function Types):  (Input) => {output}
+// 明文型別(Literal Types):            一個值本身是一個型別, 常見的是Object Literal Type
+// 通用型別(Generic Types):
+// 其他                  :             any, never, unknown,
+// 複合                                union, intersection( | 和 & )
+
