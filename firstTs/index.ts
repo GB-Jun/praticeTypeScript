0803b6884a74f21dfcb4a203883ce016485f3c45
GB-Jun <windgb369@hotmail.com>
2022-08-30 10:39:39 +0800

    update

--- firstTs/index.ts
+++ firstTs/index.ts
@@ -7,8 +7,8 @@ function say(something: string): void {
 say(message)
 
 
-const message2 = 'Hello World';
-console.log(message2.touppercase());
+// const message2 = 'Hello World';
+// console.log(message2.touppercase());
 // TSC會幫忙檢測出不相符的功能或用法, 這邊是拼寫錯誤造成func參照錯誤而產生(touppercase 應該是 toUpperCase)
 // 使用tsc去執行轉換他也會提醒錯誤, 但不會阻止轉換
 
